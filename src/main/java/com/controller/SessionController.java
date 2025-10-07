8ef2c652d5b3a617f13adaa3e9cf661c8a56a58f
Tejas Shah <tejasshah2k19@gmail.com>
2025-10-07 14:42:55 +0530

    matches

--- src/main/java/com/controller/SessionController.java
+++ src/main/java/com/controller/SessionController.java
@@ -1,6 +1,7 @@
 package com.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -18,13 +19,13 @@ public class SessionController {
 
 	@Autowired
 	PasswordEncoder encoder;
-	
+
 	@Autowired
-	JdbcTemplate stmt; 
+	JdbcTemplate stmt;
 
 	@Autowired
 	UserDao userDao;
-	
+
 	@GetMapping("register")
 	public String register() {
 		return "Register";
@@ -36,7 +37,8 @@ public class SessionController {
 	}
 
 	@PostMapping("saveuser")
-	public String saveUser(@RequestParam String firstName, String lastName, String email, String password,Model model) {
+	public String saveUser(@RequestParam String firstName, String lastName, String email, String password,
+			Model model) {
 		// read
 		System.out.println("firstName => " + firstName);
 		System.out.println(lastName);
@@ -47,33 +49,52 @@ public class SessionController {
 
 		if (firstName == null || firstName.isBlank()) {
 			isError = true;
-			model.addAttribute("firstNameError","Please Enter FirstName");
+			model.addAttribute("firstNameError", "Please Enter FirstName");
 		}
 		if (lastName == null || lastName.isBlank()) {
 			isError = true;
-			model.addAttribute("lastNameError","Please Enter LastName");
+			model.addAttribute("lastNameError", "Please Enter LastName");
 		}
 
 		if (isError == true) {
 			return "Register";
 		} else {
 
-			System.out.println("plain password -> "+password);
+			System.out.println("plain password -> " + password);
 
-			//encrypt password 
-			String encryptedPassword  = encoder.encode(password); 
-			
-			System.out.println("encrypted Password => "+encryptedPassword);
-			
-			UserBean userBean  = new UserBean();
+			// encrypt password
+			String encryptedPassword = encoder.encode(password);
+
+			System.out.println("encrypted Password => " + encryptedPassword);
+
+			UserBean userBean = new UserBean();
 			userBean.setFirstName(firstName);
 			userBean.setLastName(lastName);
 			userBean.setEmail(email);
 			userBean.setPassword(encryptedPassword);
-			
+
 			// db insert
-			//kungfu -> plain text 
+			// kungfu -> plain text
 			userDao.addUser(userBean);
+			return "Login";
+		}
+
+	}
+
+	@PostMapping("authenticate")
+	public String authenticate(String email, String password) {
+		// password=>tejas
+
+		// select * from users where email = ?
+
+		UserBean user = userDao.getUserByEmail(email);
+		if(user == null) {
+			return "Login";
+		}
+		boolean status = encoder.matches(password, user.getPassword());
+		if (status == true) {
+			return "Home";
+		} else {
 			return "Login";
 		}
 	}
--- src/main/java/com/dao/UserDao.java
+++ src/main/java/com/dao/UserDao.java
@@ -1,6 +1,7 @@
 package com.dao;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
 
@@ -15,5 +16,15 @@ public class UserDao {
 	public void addUser(UserBean userBean) {
 		stmt.update("insert into users (firstName,lastName,email,password) values (?,?,?,?)", userBean.getFirstName(),
 				userBean.getLastName(), userBean.getEmail(), userBean.getPassword());
+	}
+
+	public UserBean getUserByEmail(String email) {
+		try {
+			return stmt.queryForObject("select * from users where email = ? ",
+					new BeanPropertyRowMapper<>(UserBean.class), new Object[] { email });
+		} catch (Exception e) {
+
+		}
+		return null;
 	}
 }
